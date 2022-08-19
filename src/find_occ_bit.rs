5b1e9049c0c7fae63707e4cca437d0cf7160a7e6
joeyst <joeystenbeck@gmail.com>
2022-08-18 23:49:43 -0700

    Add find next occupied bit functions.

--- src/find_occ_bit.rs
+++ src/find_occ_bit.rs
@@ -1,11 +1,43 @@
-pub fn up(board: u64, mut bit: u64) -> u64 {
+fn general(board: u64, mut bit: u64, direction: fn(u64) -> u64) -> u64 {
   while bit != 0 {
-    bit = crate::blocked::next_bit::up(bit);
+    bit = direction(bit);
     if bit & board != 0 {
       return bit;
     }
   }
   return 0;
+}
+
+pub fn up(board: u64, mut bit: u64) -> u64 {
+  general(board, bit, crate::blocked::next_bit::up)
+}
+
+pub fn down(board: u64, mut bit: u64) -> u64 {
+  general(board, bit, crate::blocked::next_bit::down)
+}
+
+pub fn right(board: u64, mut bit: u64) -> u64 {
+  general(board, bit, crate::blocked::next_bit::right)
+}
+
+pub fn left(board: u64, mut bit: u64) -> u64 {
+  general(board, bit, crate::blocked::next_bit::left)
+}
+
+pub fn up_right(board: u64, mut bit: u64) -> u64 {
+  general(board, bit, crate::blocked::next_bit::up_right)
+}
+
+pub fn up_left(board: u64, mut bit: u64) -> u64 {
+  general(board, bit, crate::blocked::next_bit::up_left)
+}
+
+pub fn down_right(board: u64, mut bit: u64) -> u64 {
+  general(board, bit, crate::blocked::next_bit::down_right)
+}
+
+pub fn down_left(board: u64, mut bit: u64) -> u64 {
+  general(board, bit, crate::blocked::next_bit::down_left)
 }
 
 #[cfg(test)]
@@ -19,11 +51,13 @@ mod test {
       assert!(up(BOARD, 0x10000000000) == 0x1000000000000);
     }
 
+    #[test]
     fn find_two_up() {
       let BOARD: u64 = 0xFF000000000000;
       assert!(up(BOARD, 0x100000000) == 0x1000000000000);
     }
 
+    #[test]
     fn find_none_up() {
       assert!(up(0, 0x10) == 0);
     }
