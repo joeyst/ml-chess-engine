94afc5850b58de5a49b849d424dca1bdf6142af0
joeyst <joeystenbeck@gmail.com>
2022-08-19 14:26:32 -0700

    Add tests for splitting moves into separate boards.

--- src/move.rs
+++ src/move.rs
@@ -12,7 +12,7 @@ pub fn split_slice_into_slices(mut slice: u64) -> Vec<u64> {
 pub fn split_board_into_states(slice: u64, board: [u64; 13], slice_index: u8) -> Vec<[u64; 13]> {
   let mut states: Vec<[u64; 13]> = Vec::new();
   let slices: Vec<u64> = split_slice_into_slices(slice);
-  
+
   states
 }
 
@@ -22,3 +22,27 @@ pub fn wstate(board: [u64; 13]) -> Vec<[u64; 13]> {
 }
 */
 
+#[cfg(test)]
+mod test {
+  use super::*;
+  mod split_slice_into_slices_tests {
+    use super::*;
+    #[test]
+    fn splits_into_zero_slices() {
+      let slice: u64 = 0;
+      assert!(split_slice_into_slices(slice).len() == 0);
+    }
+
+    #[test]
+    fn splits_into_one_slice() {
+      let slice: u64 = 0x8000;
+      assert!(split_slice_into_slices(slice).len() == 1);
+    }
+
+    #[test] 
+    fn splits_into_multiple_slices() {
+      let slice: u64 = 0xFF00;
+      assert!(split_slice_into_slices(slice).len() == 8);
+    }
+  }
+}
