554958994ff43212e2d35fb5e7a7b0c51f3235d2
joeyst <joeystenbeck@gmail.com>
2022-08-21 19:31:51 -0700

    Add constructors for Node and Layer.

--- src/network/layer.rs
+++ src/network/layer.rs
@@ -2,17 +2,19 @@ use super::node::Node;
 
 pub struct Layer {
   nodes: Vec<Node>,
-  activation_fn: fn(f64) -> f32,
-  pub layer_number: u8
+  activation_fn: fn(f64) -> f32
 }
 
 impl Layer {
-  pub fn create(n: Vec<Node>, a_fn: fn(f64) -> f32, l_n: u8) -> Layer {
+  pub fn create(n: Vec<Node>, a_fn: fn(f64) -> f32) -> Layer {
     Layer {
       nodes: n,
-      activation_fn: a_fn,
-      layer_number: l_n
+      activation_fn: a_fn
     }
+  }
+
+  pub fn create_random_layer(nodes_in_layer: usize, nodes_in_next_layer: usize, a_fn: fn(f64) -> f32) -> Layer {
+    Layer::create(vec![Node::create_random(nodes_in_next_layer); nodes_in_layer], a_fn)
   }
 
   pub fn collect_weights(&self, node_index: u8) -> Vec<f32> {
--- src/network/node.rs
+++ src/network/node.rs
@@ -1,3 +1,6 @@
+use rand::Rng;
+
+#[derive(Clone)]
 pub struct Node {
   pub value: f32,
   pub bias: f32,
@@ -6,7 +9,29 @@ pub struct Node {
 }
 
 impl Node {
+  pub fn create(v: f32, b: f32, w: Vec<f32>) -> Node {
+    Node {
+      value: v, 
+      bias: b, 
+      weights: w
+    }
+  }
+
+  pub fn create_random(length_of_next_layer: usize) -> Node {
+    Node {
+      value: 0f32, 
+      bias: rand::thread_rng().gen_range(-1.0..1.0), 
+      weights: vec![rand::thread_rng().gen_range(-1.0..1.0); length_of_next_layer]
+    }
+  }
+
   pub fn weight_at_index(&self, index: u8) -> f32 {
     self.weights[index as usize]
+  }
+
+  pub fn print(&self) {
+    println!("Node~~");
+    println!("  Value: {:.32}", self.value);
+    println!("  Bias:  {:.32}", self.bias);
   }
 }
